57468493177d5a82c8644c7ab1ca3204f13fb4db
Carlos Bonfim <carlos_bonfim10@hotmail.com>
2020-06-13 17:12:14 -0300

    Adiciona os primeiros arquivos de projeto
    Esses sao os primeiros arquivos do projeto de exemplo do programa
    AceleraDev de DS.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="hello",
+    version="0.0.1",
+    description="Meu novo pacote",
+    author="Carlos Bonfim",
+    author_email="carlos_bonfim10@hotmail.com",
+    packages=["hello"]
+)
